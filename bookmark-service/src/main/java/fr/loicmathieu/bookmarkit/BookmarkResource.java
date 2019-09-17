8c659e09788ad22bab54351ddd942eb5cb8e7c76
Loïc Mathieu <loikeseke@gmail.com>
2019-09-17 18:06:20 +0200

    feat: solution for step 1

--- bookmark-service/src/main/java/fr/loicmathieu/bookmarkit/BookmarkResource.java
+++ bookmark-service/src/main/java/fr/loicmathieu/bookmarkit/BookmarkResource.java
@@ -1,11 +1,6 @@
 package fr.loicmathieu.bookmarkit;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.eclipse.microprofile.metrics.annotation.Counted;
-import org.eclipse.microprofile.metrics.annotation.Timed;
-import org.eclipse.microprofile.openapi.annotations.Operation;
-
-import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -28,33 +23,19 @@ import java.util.List;
 public class BookmarkResource {
     @ConfigProperty(name="greeting") String greeting;
 
-    @PostConstruct
-    void init(){
-        System.out.println("Hello " + greeting);
-    }
-
     @GET
-    @Operation(summary = "List all bookmarks")
-    @Counted(name = "listAll.count")
-    @Timed(name="listAll.time")
     public List<Bookmark> listAll(){
         return Bookmark.listAll();
     }
 
     @GET
     @Path("/{id}")
-    @Operation(summary = "Get a bookmark")
-    @Counted(name = "get.count")
-    @Timed(name="get.time")
     public Bookmark get(@PathParam("id") Long id) {
         return Bookmark.findById(id);
     }
 
     @POST
     @Transactional
-    @Operation(summary = "Create a bookmark")
-    @Counted(name = "create.count")
-    @Timed(name="create.time")
     public Response create(Bookmark bookmark){
         bookmark.persist();
         return Response.created(URI.create("/bookmarks/" + bookmark.id)).build();
@@ -63,9 +44,6 @@ public class BookmarkResource {
     @PUT
     @Path("{id}")
     @Transactional
-    @Operation(summary = "Update a bookmark")
-    @Counted(name = "update.count")
-    @Timed(name="update.time")
     public void update(Bookmark bookmark){
         Bookmark existing = Bookmark.findById(bookmark.id);
         existing.url = bookmark.url;
@@ -77,9 +55,6 @@ public class BookmarkResource {
     @DELETE
     @Path("{id}")
     @Transactional
-    @Operation(summary = "Delete a bookmark")
-    @Counted(name = "delete.count")
-    @Timed(name="delete.time")
     public void delete(@PathParam("id")Long id){
         if (bookmark != null) {
             bookmark.delete();
