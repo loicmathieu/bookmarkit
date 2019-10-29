f85d4496dc8c33b49ab27dec90ae58233e8e468c
Sparow199 <mf.aoudia@gmail.com>
2019-10-29 01:13:57 +0100

    feat(*): step 5
    feat(*): Add step 5

--- geoip-service/src/main/java/com/lahzouz/bookmarkit/GeoIp.java
+++ geoip-service/src/main/java/com/lahzouz/bookmarkit/GeoIp.java
@@ -1,24 +1,20 @@
 package com.lahzouz.bookmarkit;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class GeoIp {
-    public String ip;
-    @JsonProperty("country_code")
+
+    public String query;
+    public String status;
+    public String country;
     public String countryCode;
-    @JsonProperty("country_name")
-    public String countryName;
-    @JsonProperty("region_code")
-    public String regionCode;
-    @JsonProperty("region_name")
+    public String region;
     public String regionName;
     public String city;
-    @JsonProperty("zip_code")
-    public String zipCode;
-    @JsonProperty("time_zone")
-    public String timeZone;
-    public Double latitude;
-    public Double longitude;
-    @JsonProperty("metro_code")
-    public Integer metroCode;
+    public String zip;
+    public Double lat;
+    public Double lon;
+    public String timezone;
+    public String isp;
+    public String org;
+    public String as;
+
 }
