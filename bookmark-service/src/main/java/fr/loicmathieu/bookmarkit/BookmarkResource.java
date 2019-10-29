5c966a0358b45e3786509b3f03c807501a0affe1
Sparow199 <mf.aoudia@gmail.com>
2019-10-29 01:12:36 +0100

    feat(*): step 1
    feat(*): Add step 5
    
    
    feat(*): Add step 3
    
    
    feat(*) Add step-2
    
    
    feat(*): Add step-1

--- bookmark-service/src/main/java/fr/loicmathieu/bookmarkit/BookmarkResource.java
+++ bookmark-service/src/main/java/fr/loicmathieu/bookmarkit/BookmarkResource.java
@@ -1,11 +1,20 @@
 package fr.loicmathieu.bookmarkit;
 
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 import java.util.List;
 
 @Path("/bookmarks")
@@ -13,23 +22,44 @@ import java.util.List;
 @Consumes(MediaType.APPLICATION_JSON)
 public class BookmarkResource {
 
-    public List<Bookmark> listAll(){
-        throw new UnsupportedOperationException("not yet implemented");
+    @GET
+    public List<Bookmark> listBookmarks() {
+        return Bookmark.listAll();
     }
 
-    public Bookmark get(Long id) {
-        throw new UnsupportedOperationException("not yet implemented");
+    @GET
+    @Path("{id}")
+    public Bookmark getBookmark(@PathParam("id") Long id) {
+        return Bookmark.findById(id);
     }
 
-    public Response create(Bookmark bookmark){
-        throw new UnsupportedOperationException("not yet implemented");
+    @POST
+    @Transactional
+    public Response createBookmark(@Valid Bookmark bookmark) {
+            bookmark.persist();
+            return Response.status(Response.Status.CREATED).entity(bookmark).build();
     }
 
-    public void update(Bookmark bookmark){
-        throw new UnsupportedOperationException("not yet implemented");
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public Response updateBookmark(@Valid Bookmark bookmark, @PathParam("id") Long id, @Context UriInfo uriInfo) {
+        Bookmark entity = Bookmark.findById(id);
+        entity.description = bookmark.description;
+        entity.location = bookmark.location;
+        entity.title = bookmark.title;
+        entity.url = bookmark.url;
+        return Response.created(URI.create(uriInfo.getPath())).build();
     }
 
-    public void delete(Long id){
-        throw new UnsupportedOperationException("not yet implemented");
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public Response deleteBookmark(@PathParam("id") Long id) {
+        Bookmark bookmark = Bookmark.findById(id);
+        if (bookmark.isPersistent()) {
+            bookmark.delete();
+        }
+        return Response.noContent().build();
     }
 }
