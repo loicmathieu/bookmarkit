0f77031ca27f905bce1c1ed320c63bdcd82d7b26
Loïc Mathieu <loikeseke@gmail.com>
2020-05-24 17:55:56 +0200

    feat: solution for step 1

--- bookmark-service/src/main/java/fr/loicmathieu/bookmarkit/Bookmark.java
+++ bookmark-service/src/main/java/fr/loicmathieu/bookmarkit/Bookmark.java
@@ -1,9 +1,11 @@
 package fr.loicmathieu.bookmarkit;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
 import javax.persistence.Entity;
 
 @Entity
-public class Bookmark {
+public class Bookmark extends PanacheEntity {
     public String title;
     public String url;
     public String description;
