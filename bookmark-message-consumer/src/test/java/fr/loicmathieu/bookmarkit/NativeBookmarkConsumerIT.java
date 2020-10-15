d08e45a98aae91db4b4a10b00741794039760ef1
Loïc Mathieu <loikeseke@gmail.com>
2020-10-15 14:19:15 +0200

    chore: upgrade to Quarkus 1.8.3 and Java 11

--- bookmark-message-consumer/src/test/java/fr/loicmathieu/bookmarkit/NativeBookmarkConsumerIT.java
+++ bookmark-message-consumer/src/test/java/fr/loicmathieu/bookmarkit/NativeBookmarkConsumerIT.java
@@ -1,8 +1,8 @@
 package fr.loicmathieu.bookmarkit;
 
-import io.quarkus.test.junit.SubstrateTest;
+import io.quarkus.test.junit.NativeImageTest;
 
-@SubstrateTest
+@NativeImageTest
 public class NativeBookmarkConsumerIT extends BookmarkConsumerTest {
 
     // Execute the same tests but in native mode.
